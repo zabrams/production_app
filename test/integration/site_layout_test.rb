23b5e5296860f12989c012a2681c525aa3e801ee
zabrams <zachary.abrams@gmail.com>
2015-05-23 15:26:08 -0700

    Add small tests

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -13,4 +13,9 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   	assert_select "a[href=?]", about_path
     assert_select "a[href=?]", signup_path
   end
+
+  test "signup title" do
+    get signup_path
+    assert_select "title", 'Sign up'
+  end
 end
