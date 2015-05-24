e6a84567ed6efc9e35b861402ef4b4182b16ebab
zabrams <zachary.abrams@gmail.com>
2015-05-24 09:57:41 -0700

    Add tests for name and email. Migrate db to inc email index

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,58 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  def setup
+  	@user = User.new(name: "Zach Abrams", email: "zach@email.com")
+  end
+
+  test "should be valid" do
+  	assert @user.valid?
+  end
+
+  test "name should be present" do
+  	@user.name = ""
+  	assert_not @user.valid? 
+  end
+
+  test "should have email" do
+  	@user.email = ""
+  	assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+  	@user.name = "a" * 51
+  	assert_not @user.valid?
+  end
+
+  test "email should not be too long" do
+  	@user.email = "a" * 244 + "@example.com"
+  	assert_not @user.valid?
+  end
+
+  test "email validation should accept valid emails" do
+  	valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                         first.last@foo.jp alice+bob@baz.cn]
+  	valid_addresses.each do |address|
+  		@user.email = address
+  		assert @user.valid?, "#{address.inspect} should be valid"
+  	end
+  end
+
+  test "email validation should not accept invalid emails" do
+  	invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                           foo@bar_baz.com foo@bar+baz.com]
+    invalid_addresses.each do |address|
+    	@user.email = address
+    	assert_not @user.valid?, "#{address.inspect} should not be valid"
+    end
+  end
+
+  test "email should be unique" do
+  	duplicate_user = @user.dup
+  	duplicate_user.email = @user.email.upcase
+  	@user.save
+  	assert_not duplicate_user.valid? 
+  end
+
 end
