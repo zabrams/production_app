fbc986ed9baed37b2fb7d4213da9e83595a8538c
zabrams <zachary.abrams@gmail.com>
2015-05-24 10:23:00 -0700

    Add password validation

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   
   def setup
-  	@user = User.new(name: "Zach Abrams", email: "zach@email.com")
+  	@user = User.new(name: "Zach Abrams", email: "zach@email.com", 
+                      password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -53,6 +54,16 @@ class UserTest < ActiveSupport::TestCase
   	duplicate_user.email = @user.email.upcase
   	@user.save
   	assert_not duplicate_user.valid? 
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 
 end
