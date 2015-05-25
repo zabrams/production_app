d1fecf71b3f439ab701c58eb48e299cbc152c14d
zabrams <zachary.abrams@gmail.com>
2015-05-25 16:08:46 -0700

    Add logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -19,10 +19,18 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
   test "should properly log in user" do
   	get login_path
   	post_via_redirect login_path, session: { email: @user.email, password: 'password' }
+  	assert is_logged_in?
   	assert_template 'users/show'
   	assert_select "a[href=?]", login_path, count: 0
   	assert_select "a[href=?]", user_path(@user)
   	assert_select "a[href=?]", logout_path
+  	delete logout_path
+  	assert_not is_logged_in?
+  	assert_redirected_to root_url
+  	follow_redirect!
+  	assert_select "a[href=?]", login_path
+  	assert_select "a[href=?]", user_path(@user), count: 0
+  	assert_select "a[href=?]", logout_path, count: 0
   end
 
 end
