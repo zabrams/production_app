9c58f8077d27c0b28d2cc5060f5dec8fbdcdd706
zabrams <zachary.abrams@gmail.com>
2015-05-25 09:12:24 -0700

    Add error handling for sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,18 @@ class UsersController < ApplicationController
   	#debugger
   end 
 
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to show_path
+  	else
+  		render 'new'
+  	end
+  end
+
+  private
+  	def user_params
+  		params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  	end
+  	
 end
