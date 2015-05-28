ac02e86a8c9d37f1665a5c6fee3011ada9971a00
zabrams <zachary.abrams@gmail.com>
2015-05-28 13:27:48 -0700

    Add users index

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -39,4 +39,9 @@ class UsersControllerTest < ActionController::TestCase
   	assert_redirected_to root_url
   end
 
+  test "shold redirect index when not logged in" do
+  	get 'index'
+  	assert_redirected_to login_url
+  end
+
 end
