3e443d9424f2dc6724aaafeaca3268227e02bf9b
zabrams <zachary.abrams@gmail.com>
2015-05-31 13:56:59 -0700

    Add reset emails

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   
+  get 'password_resets/new'
+
+  get 'password_resets/edit'
+
   get 'sessions/new'
 
   root 'static_pages#home'
@@ -16,6 +20,8 @@ Rails.application.routes.draw do
   delete 'logout' => 'sessions#destroy'
 
   resources :account_activations, only: [:edit]
+
+  resources :password_resets, only: [:new, :edit, :create, :update]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
