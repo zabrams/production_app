bd1a173c4b78013a338b0c4426337671130ca190
zabrams <zachary.abrams@gmail.com>
2015-06-02 08:11:23 -0700

    Depened destory micropost to user

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -77,4 +77,12 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.authenticated?(:remember, '')
   end
 
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem Ipsum")
+    assert_difference "Micropost.count", -1 do
+      @user.destroy
+    end
+  end
+
 end
