f448abbb281457cb2b813ed97c39f1590a84d287
zabrams <zachary.abrams@gmail.com>
2015-06-03 22:29:26 -0700

    add create destory

--- config/routes.rb
+++ config/routes.rb
@@ -23,6 +23,8 @@ Rails.application.routes.draw do
 
   resources :password_resets, only: [:new, :edit, :create, :update]
 
+  resources :microposts, only: [:create, :destroy]
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
